727dcad99d71b9eca6d08335a726f175a905ba2c
Sebastião Marcos <sebastiaomarcos49@gmail.com>
2022-01-22 17:12:50 -0300

    Inserção de usuário com POST

--- src/main/java/com/smarcosm/workshopmongo/service/UserService.java
+++ src/main/java/com/smarcosm/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.smarcosm.workshopmongo.domain.User;
+import com.smarcosm.workshopmongo.dto.UserDTO;
 import com.smarcosm.workshopmongo.repository.UserRepository;
 import com.smarcosm.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -25,5 +26,15 @@ public class UserService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		
+	}
+	
+	
 }
